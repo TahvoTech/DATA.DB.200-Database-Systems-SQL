f741d08d578cc666bfdc103e3d0f493f6623fc53
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-11-01 12:54:06 +0200

    Add SQL query to retrieve distinct customer IDs and names from joined tables

--- 02_Pakolliset tehtävät/01_SQL-kertaus/07_tehtava.sql
+++ 02_Pakolliset tehtävät/01_SQL-kertaus/07_tehtava.sql
@@ -10,3 +10,7 @@ Tulostaulun sarakenimet
 asiakas_id nimi
 */
 
+SELECT DISTINCT asiakas.asiakas_id, asiakas.nimi
+FROM asiakas
+JOIN tilaus ON asiakas.asiakas_id = tilaus.asiakas_id  -- Inner join palauttaa vain ne rivit, joilla on yhteinen arvo molemmissa tauluissa.
+ORDER BY asiakas.asiakas_id
