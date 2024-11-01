db536a954f7ac6e9e3aa929a48d28cf35db1860c
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-11-01 16:19:14 +0200

    Add initial SQL exercise file and UML diagram description

---
+++ 02_Viikkoharjoitukset/01_week/01_viikko_01_harjoitus.sql
@@ -0,0 +1,18 @@
+-- Jarmo Tahvanainen
+-- jarmo.tahvanainen@tuni.fi
+
+/*
+Tehtävä 1: 
+
+Alla on annettu UML-kaavio, jossa on kuvattu kolme luokkaa, sekä näiden
+väliset assosiaatiot. Kaaviossa tunnisteen eteen on lisätty # (huom: ei tyypillinen UML
+notaatio).
+
+a) Mitä oletuksia lukumääräsuhteissa on tehty? (esim: Kuinka monta työntekijää on
+töissä huoltofirmassa?). Kaaviossa käytetään UML-lyhenteitä: 1 = (1,1) sekä * = (0,*).
+
+b) Esitä vastaava ER-kaavio
+
+c) Kuvaa UML-kaavio relaatiokannan kaavioksi.
+*/
+
