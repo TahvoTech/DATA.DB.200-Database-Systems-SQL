2669e58b558ff6bba6e66e1f2f50cf3b0c0c0d4a
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-11-01 15:05:04 +0200

    Add SQL query to retrieve customers who ordered both 'rihveli' and 'tunkki'

--- 02_Pakolliset tehtävät/01_SQL-kertaus/11_tehtava.sql
+++ 02_Pakolliset tehtävät/01_SQL-kertaus/11_tehtava.sql
@@ -8,3 +8,15 @@ Hae asiakas_id ja nimi niille asiakkaille, jotka ovat tilanneet sekä rihvelin e
 Tulostaulun sarakenimet:
 asiakas_id nimi
 */
+
+SELECT asiakas.asiakas_id, asiakas.nimi
+FROM asiakas
+JOIN tilaus
+ON asiakas.asiakas_id = tilaus.asiakas_id
+JOIN tilaustuote
+ON tilaus.tilaus_id = tilaustuote.tilaus_id
+JOIN tuote
+ON tilaustuote.tuote_id = tuote.tuote_id
+WHERE tuote.nimi IN ('rihveli', 'tunkki')
+GROUP BY asiakas.asiakas_id, asiakas.nimi -- yhdistetään, jotta voidaan laskea montako eri tuotetta asiakas on tilannut
+HAVING COUNT(DISTINCT tuote.nimi) = 2  -- lasketaan montako eri tuotetta asiakas on tilannut. Täsä tapauksessa pitää olla 2 eri tuotetta.
