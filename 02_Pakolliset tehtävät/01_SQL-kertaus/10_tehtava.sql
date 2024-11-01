b56989e95e87d2101639ef03f9ef89ba82a16665
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-11-01 14:49:49 +0200

    Add SQL query to count customers with more than one order

--- 02_Pakolliset tehtävät/01_SQL-kertaus/10_tehtava.sql
+++ 02_Pakolliset tehtävät/01_SQL-kertaus/10_tehtava.sql
@@ -11,3 +11,16 @@ Tulostaulun sarakenimet:
 asiakas_id tilaus_lkm
 */
 
+SELECT asiakas.asiakas_id, COUNT(tilaus.tilaus_id) AS tilaus_lkm
+FROM asiakas
+JOIN tilaus -- ei kaikkia näkyviin, joten JOIN. LEFT JOIN ei tarpeen.
+ON asiakas.asiakas_id = tilaus.asiakas_id -- tämä koska tarvitaan vain ne, joilla on tilauksia
+GROUP BY asiakas.asiakas_id
+HAVING COUNT(tilaus.tilaus_id) > 1 -- tällä saadaan ne, joilla on useampi kuin yksi tilaus, eli sortattua pois ne, joilla on vain yksi tilaus.
+ORDER BY asiakas.asiakas_id;
+
+-- pitäisi näyttää tältä:
+-- 
+-- | asiakas_id | tilaus_lkm |
+-- |------------|------------|
+-- | 3          | 2          |
