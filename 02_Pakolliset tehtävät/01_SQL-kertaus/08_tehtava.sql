7f6f5d8ef5c3f18f676aa53c83d157da92163b4f
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-11-01 13:02:49 +0200

    Add SQL query to retrieve products not included in any orders

--- 02_Pakolliset tehtävät/01_SQL-kertaus/08_tehtava.sql
+++ 02_Pakolliset tehtävät/01_SQL-kertaus/08_tehtava.sql
@@ -9,3 +9,9 @@ Lajittele tulostaulun rivit nousevaan järjestykseen tuote_id:n perusteella.
 Tulostaulun sarakenimet:
 tuote_id nimi valmistaja hinta
 */
+
+SELECT tuote.tuote_id, tuote.nimi, tuote.valmistaja, tuote.hinta
+FROM tuote
+LEFT JOIN tilaustuote ON tuote.tuote_id = tilaustuote.tuote_id  -- käyttäessä left joinia, niin lisää tuote taulun kaikki rivit, eli myös tuote_id:t, jotka eivät ole tilaustuotteissa. Sitten Where avulla valitaan ne tuotteet, jotka eivät ole tilaustuotteissa.
+WHERE tilaustuote.tuote_id IS NULL
+ORDER BY tuote.tuote_id;
