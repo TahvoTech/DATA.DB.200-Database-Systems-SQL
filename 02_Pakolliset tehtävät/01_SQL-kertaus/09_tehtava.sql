1f14aa17fd2979b0b0033d3010484f2ec25d58a8
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-11-01 13:11:46 +0200

    Add SQL query to count orders per customer with left join

--- 02_Pakolliset tehtävät/01_SQL-kertaus/09_tehtava.sql
+++ 02_Pakolliset tehtävät/01_SQL-kertaus/09_tehtava.sql
@@ -10,3 +10,21 @@ Lajittele tulostaulun rivit nousevaan järjestykseen asiakas_id:n perusteella.
 Tulostaulun sarakenimet:
 asiakas_id nimi tilaus_lkm
 */
+
+SELECT asiakas.asiakas_id, asiakas.nimi, COUNT(tilaus.tilaus_id) AS tilaus_lkm
+FROM asiakas
+LEFT JOIN tilaus
+ON asiakas.asiakas_id = tilaus.asiakas_id
+GROUP BY asiakas.asiakas_id
+ORDER BY asiakas.asiakas_id;
+
+/*
+ * This SQL query selects all columns from the "employees" table
+ * where the "department" column matches the specified department name.
+ * 
+ * Parameters:
+ * - department_name: The name of the department to filter employees by.
+ * 
+ * Example usage:
+ * SELECT * FROM employees WHERE department = 'Sales';
+ */
